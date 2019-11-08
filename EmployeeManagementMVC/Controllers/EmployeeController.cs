270c7ee5b55568c29c75c8899272254234c15b49
lek <lek@thebuffalogroup.com>
2019-11-08 06:27:59 -0800

    first commit

---
+++ EmployeeManagementMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,56 @@
+﻿using EmployeeManagementMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeManagementMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeManagementEntities _context = new EmployeeManagementEntities();
+
+        // GET All Employees
+        public ActionResult Index()
+        {
+            List<Employee> employeeList = _context.Employees.ToList();
+
+            EmployeeViewModel employeeVM = new EmployeeViewModel();
+
+            List<EmployeeViewModel> employeeVMList = employeeList.Select(x => new EmployeeViewModel
+            {
+                EmployeeId = x.EmployeeId,
+                Name = x.Name,
+                DepartmentId = x.DepartmentId,
+                Address = x.Address,
+                DepartmentName = x.Department.DepartmentName
+            }).ToList();
+
+            return View(employeeVMList);
+        }
+
+        public ActionResult EmployeeDetail(int id)
+        {
+            Employee employee = _context.Employees.FirstOrDefault(x => x.EmployeeId == id);
+
+            EmployeeViewModel employeeVM = new EmployeeViewModel()
+            {
+                EmployeeId = employee.EmployeeId,
+                Name = employee.Name,
+                Address = employee.Address,
+                DepartmentId = employee.DepartmentId,
+                DepartmentName = employee.Department.DepartmentName
+            };
+            
+            return View(employeeVM);
+        }
+
+        public ActionResult CreateEmployee()
+        {
+
+            return View("CreateEmployee");
+        }
+
+}
+}
---
+++ EmployeeManagementMVC/Models/EmployeeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeManagementMVC.Models
+{
+    public class EmployeeViewModel
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public Nullable<int> DepartmentId { get; set; }
+        public string Address { get; set; }
+
+        public string DepartmentName { get; set; }
+    }
+}
